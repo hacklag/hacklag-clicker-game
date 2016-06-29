9644f096469d5c6c29ed3395fce2959e9c12bc45
dragon <cyandragon@protonmail.com>
2016-06-29 12:36:45 +0200

    [dragon] Added some changes

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Radium from 'radium';
 class App extends React.Component {
   constructor(){
     super();
     this.state = { val: 0};
     this.update = this.update.bind(this);
+  }
+  getStyles() {
+    return {
+      mainDiv: {
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      hackbatlogo: {
+        display: 'block',
+        margin: 'auto',
+        width: 321,
+        border: '1px solid'
+      },
+      counter: {
+        justifyContent: 'flex-start',
+        width: 100,
+        border: '1px solid',
+      },
+      smallbat: {
+        //marginTop: 6,
+        width: 30,
+        height: 'auto',
+        border: '0.5px solid'
+      }
+  }
+
   }
   update(){
     this.setState({val: this.state.val + 1})
@@ -13,13 +40,22 @@ class App extends React.Component {
     console.log('mounting')
   }
   render(){
+    const styles = this.getStyles();
+
     return(
-      <div>
-        <img
-          onClick={this.update}
-          src='/Icons/tak.png' />
-        <br />
-        {this.state.val}
+      <div style={styles.mainDiv}>
+        <div style={styles.counter} >
+          <img
+            style={styles.smallbat}
+            src='/Icons/hackbat.png' />
+          {this.state.val}
+        </div>
+        <div >
+          <img
+            style={styles.hackbatlogo}
+            onClick={this.update}
+            src='/Icons/hackbat.png' />
+        </div>
       </div>
     );
   }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Radium from 'radium';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'))
