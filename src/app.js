6bc59c2a8097a476f209e61de86b6601f88ead08
dragon <cyandragon@protonmail.com>
2016-07-11 21:19:54 +0200

    [devel] Added new awesome features

--- src/app.js
+++ src/app.js
@@ -4,5 +4,7 @@ import Radium from 'radium';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './apps/App';
 import ResetDialog from './apps/dialog.js';
+//import FlatButton from 'material-ui/FlatButton';
+//import SynLog from './apps/synlog/apiconnect.js';
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- src/apps/App.js
+++ src/apps/App.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import Radium from 'radium';
 import ResetDialog from './dialog.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Numeral from 'numeral';
+import FlatButton from 'material-ui/FlatButton';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 //import Shop from './shop.js';
 
 class App extends React.Component{
+  getChildContext() {
+    return { muiTheme: getMuiTheme(baseTheme) };
+  }
   constructor(){
     super();
     this.state = {
@@ -16,56 +23,11 @@ class App extends React.Component{
     App.reset = this.reset.bind(this);
   }
 
-  getStyles() {
-    return {
-      mainDiv: {
-        display: 'flex',
-        flexDirection: 'column'
-      },
-      hackbatlogo: {
-        display: 'block',
-        margin: 'auto',
-        width: 322,
-        border: '1px solid'
-      },
-      counter: {
-        position: 'absolute',
-        backgroundColor: 'rgba(255,255,255,0.75)',
-        borderRadius: '18px',
-        height: '28px',
-        padding: '4px 4px 0px 4px',
-        fontWeight: 'bold',
-        fontFamily: 'Courier new.monospace',
-        fontSize: '24px'
-      },
-      smallbat: {
-        width: 32,
-        height: 'auto',
-      },
-      reset: {
-        marginTop: 37
-      },
-      shop: {
-        border: '0.5px solid',
-        position: 'absolute',
-        right: 0,
-        height: 600,
-        width: 200,
-        marginTop: -206
-      },
-      shoprow: {
-        height: 74,
-        width: 201,
-        right: 0,
-        borderBottom: '1px solid'
-      }
-    }
-  }
   update(){
     this.setState({
       val: this.state.val + 1
     })
-    document.title = `${this.state.val} Hackbats - Hackbat Clicker`;
+    document.title = `${Numeral(this.state.val).format('0,0')} Hackbats - Hackbat Clicker`;
   }
 
   componentWillMount(){
@@ -77,7 +39,6 @@ class App extends React.Component{
   }
 
   reset() {
-    //console.debug("woop");
     this.setState({
       val: 0
     });
@@ -85,7 +46,7 @@ class App extends React.Component{
   }
 
   render(){
-    const {mainDiv, counter, smallbat, hackbatlogo, reset, shop, shoprow} = this.getStyles();
+    const {button, shopbat, mainDiv, counter, smallbat, hackbatlogo, leftpanel, shop, shoprow, hackbatdiv, shopicon, shophead} = this.getStyles();
 
     return(
       <div style={mainDiv}>
@@ -93,13 +54,17 @@ class App extends React.Component{
           <img
             style={smallbat}
             src={require('../assets/Icons/hackbat.png')} />
-          {this.state.val}
+          {Numeral(this.state.val).format('0,0')}
         </div>
-        <div style={reset}>
+        <div style={leftpanel}>
           <ResetDialog />
-          <button onClick={ResetDialog.handleOpen}>Reset</button>
+          <div id="hps">hackbats/second : 1</div>
+          <br/>
+          <div style={button} onClick={ResetDialog.handleOpen}>Reset</div>
+          <br/>
+          <div id='version'>running v.0.1</div>
         </div>
-        <div>
+        <div style={hackbatdiv}>
           <img
             style={hackbatlogo}
             onClick={this.update}
@@ -107,37 +72,175 @@ class App extends React.Component{
         </div>
         <div style={shop}>
           <div style={shoprow}>
-            Cursor
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              Cursor - <img style={shopbat} src={require('../assets/Icons/hackbat.png')} />
+            </div>
           </div>
           <div style={shoprow}>
-            Cave
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              Cave
+            </div>
           </div>
           <div style={shoprow}>
-            3
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              3
+            </div>
           </div>
           <div style={shoprow}>
-            4
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              4
+            </div>
           </div>
           <div style={shoprow}>
-            5
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              5
+            </div>
           </div>
           <div style={shoprow}>
-            6
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              6
+            </div>
           </div>
           <div style={shoprow}>
-            7
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              7
+            </div>
           </div>
-          <div>
-            8
+          <div style={shoprow}>
+            <div>
+              <img
+                style={shopicon}
+                src={require('../assets/Icons/cursor.svg')} />
+            </div>
+            <div style={shophead}>
+              8
+            </div>
           </div>
         </div>
       </div>
     );
   };
+  getStyles() {
+    return {
+      mainDiv: {
+        display: 'flex',
+        flexDirection: 'column',
+        fontFamily: 'Roboto',
+      },
+      hackbatdiv: {
+        border: '1px solid',
+        margin: '100px auto'
+      },
+      hackbatlogo: {
+        display: 'block',
+        width: 240,
+        cursor: 'pointer'
+      },
+      counter: {
+        position: 'absolute',
+        backgroundColor: 'rgba(255,255,255,0.75)',
+        borderRadius: '18px',
+        height: '24px',
+        padding: '4px 8px 4px 4px',
+        fontSize: '24px',
+        fontWeight: 'bold',
+        margin: '10px 10px'
+      },
+      smallbat: {
+        width: 32,
+        height: 'auto',
+      },
+      leftpanel: {
+        position: 'absolute',
+        top: '45px',
+        padding: '2px 6px',
+        margin: '10px 10px',
+        backgroundColor: 'rgba(255,255,255,0.75)',
+        borderRadius: '8px',
+        lineHeight: '125%',
+        zIndex: '100',
+        fontSize: '12px',
+        fontWeight: 'bold'
+      },
+      button: {
+        textDecoration: 'underline',
+        cursor: 'pointer',
+        fontSize: '14px'
+      },
+      shop: {
+        border: '0.5px solid',
+        position: 'absolute',
+        right: 0,
+        height: 536,
+        width: 320,
+        top: 0
+      },
+      shoprow: {
+        height: 64,
+        width: 'auto',
+        right: 0,
+        borderBottom: '1px solid',
+        borderTop: '2px solid',
+        backgroundColor: '#eee'
+      },
+      shopicon: {
+        width: '60px',
+        position: 'absolute',
+        borderRight: '1px solid'
+      },
+      shophead: {
+        fontWeight: 'bold',
+        marginLeft: '64px',
+        marginTop: '1px'
+      },
+      shopbat: {
+        width: '20px'
+      }
+    }
+  }
 
   componentDidMount(){
     this.timer = setInterval(this.update, this.state.timeOffset);
   }
 
 }
+App.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
+};
+
 export default Radium(App);
